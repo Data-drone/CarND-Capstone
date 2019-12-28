188f35ab053c2288760315b4ef54f4342091aa72
Data-drone <bpl.law@gmail.com>
2019-12-28 14:52:53 +1100

    started on tf tl_detector

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,24 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        self.MODEL_FILE = ''
+
+        self.model_graph = self.load_graph(self.MODEL_FILE)
+
+
+    def load_graph(self, graph_file):
+        graph = tf.Graph()
+        with graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(graph_file, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        return graph
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +31,5 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
+
         return TrafficLight.UNKNOWN
