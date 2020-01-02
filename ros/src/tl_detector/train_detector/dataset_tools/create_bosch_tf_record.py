5a078fc62f088bfd863b5354a375b738468dcede
Data-drone <bpl.law@gmail.com>
2020-01-03 10:43:16 +1100

    got tf record converter working

--- ros/src/tl_detector/train_detector/dataset_tools/create_bosch_tf_record.py
+++ ros/src/tl_detector/train_detector/dataset_tools/create_bosch_tf_record.py
@@ -12,7 +12,7 @@ from lxml import etree
 import PIL.Image
 import tensorflow as tf
 
-#from object_detection.utils import dataset_util
+import dataset_util
 #from object_detection.utils import label_map_util
 
 flags = tf.app.flags
@@ -24,6 +24,10 @@ flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
 FLAGS = flags.FLAGS
 
 SETS = ['train', 'test']
+
+bosch_label_dict = {'GreenStraightRight': 1, 'Red': 2, 'Yellow':3, 'RedStraightLeft':4, 
+                    'RedLeft':5, 'GreenStraight':6, 'RedStraight': 7, 'RedRight': 8, 'off': 9,
+                    'GreenStraightLeft': 10, 'GreenLeft': 11, 'GreenRight': 12, 'Green': 13}
 
 def recursive_parse_xml_to_dict(xml):
     # from tf object detection api dataset_utils
@@ -101,7 +105,8 @@ def dict_to_tf_example(data,
     poses = []
     difficult_obj = []
   
-    for obj in data['object']
+    
+    for obj in data['object']:
         difficult = bool(int(obj['difficult']))
         if ignore_difficult_instances and difficult:
             continue
@@ -113,64 +118,72 @@ def dict_to_tf_example(data,
         xmax.append(float(obj['bndbox']['xmax']) / width)
         ymax.append(float(obj['bndbox']['ymax']) / height)
         classes_text.append(obj['name'].encode('utf8'))
-        #classes.append(label_map_dict[obj['name']])
+        classes.append(bosch_label_dict[obj['name']])
         #truncated.append(int(obj['truncated']))
         poses.append(obj['pose'].encode('utf8'))
 
         example = tf.train.Example(features=tf.train.Features(feature={
-      'image/height': dataset_util.int64_feature(height),
-      'image/width': dataset_util.int64_feature(width),
-      'image/filename': dataset_util.bytes_feature(
-          data['filename'].encode('utf8')),
-      'image/source_id': dataset_util.bytes_feature(
-          data['filename'].encode('utf8')),
-      'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
-      'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-      'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
-      'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
-      'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
-      'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
-      'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
-      'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-      'image/object/class/label': dataset_util.int64_list_feature(classes), # WE NEED SORT THIS OUT
-      'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
-      'image/object/truncated': dataset_util.int64_list_feature(truncated),
-      'image/object/view': dataset_util.bytes_list_feature(poses),
-    }))
+            'image/height': dataset_util.int64_feature(height),
+            'image/width': dataset_util.int64_feature(width),
+            'image/filename': dataset_util.bytes_feature(
+                data['filename'].encode('utf8')),
+            'image/source_id': dataset_util.bytes_feature(
+                data['filename'].encode('utf8')),
+            'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
+            'image/encoded': dataset_util.bytes_feature(encoded_png),
+            'image/format': dataset_util.bytes_feature('png'.encode('utf8')),
+            'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
+            'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
+            'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
+            'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
+            'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+            'image/object/class/label': dataset_util.int64_list_feature(classes), 
+            'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
+            #'image/object/truncated': dataset_util.int64_list_feature(truncated),
+            'image/object/view': dataset_util.bytes_list_feature(poses),
+        }))
+
+
     return example
 
     
 def main(_):
-    if FLAGS.set not in SETS:
+    if FLAGS.subset not in SETS:
         raise ValueError('set must be in : {}'.format(SETS))
 
-    data_dir = FLAGS.data_dir
+    data_dir = os.path.join(FLAGS.data_dir, 'dataset_' + FLAGS.subset + '_rgb')
+
     writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
 
     #label_map_dict = label_map_util.get_label_map_dict(FLAGS.label_map_path)
 
     logging.info('Reading bosch dataset')
+
+    
+
     annotations_path = os.path.join(data_dir, 'annotations') # need check this
 
     annotations_list = os.listdir(annotations_path)
 
-    data_path = os.path.join(data_dir, 'rgb', FLAGS.set)
+    data_path = os.path.join(data_dir, 'rgb', FLAGS.subset)
     data_dict = generate_data_dict(data_path)
 
     for idx, _file in enumerate(annotations_list):
         if idx % 100 == 0:
-            logging.info('On image {0} of {1}'.format(idx, len(_file)))
+            logging.info('On image {0} of {1}'.format(idx, len(annotations_list)))
 
-        #print('{0}'.format(_file))
         path = os.path.join(annotations_path, _file)
         with tf.gfile.GFile(path, 'r') as fid:
             xml_str = fid.read()
         xml = etree.fromstring(xml_str)
 
         # we need to find the data... hmmmm.... 
-        data = recursive_parse_xml_to_dict(xml)['annotation']
+        data = recursive_parse_xml_to_dict(xml)['annotaion']
 
-        tf.example = dict_to_tf_example(data, data_dict)
+        if 'object' in data.keys():
+            tf_example = dict_to_tf_example(data, data_dict, label_map_dict=None)
+
+            writer.write(tf_example.SerializeToString())
 
     writer.close()
 
