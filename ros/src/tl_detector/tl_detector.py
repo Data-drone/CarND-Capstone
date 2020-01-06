eda4fb97731994f69635ea7787b78eee609ca521
Data-drone <bpl.law@gmail.com>
2020-01-06 23:03:56 +1100

    fixed indent issues with tl_detector

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -90,38 +90,38 @@ class TLDetector(object):
         self.has_image = True
         self.camera_image = msg
 
-        #if self.image_buffer == 0:
-        light_wp, state = self.process_traffic_lights()
+        if self.image_buffer == 0:
+            light_wp, state = self.process_traffic_lights()
 
-        '''
-        Publish upcoming red lights at camera frequency.
-        Each predicted state has to occur `STATE_COUNT_THRESHOLD` number
-        of times till we start using it. Otherwise the previous stable state is
-        used.
-        '''
+            '''
+            Publish upcoming red lights at camera frequency.
+            Each predicted state has to occur `STATE_COUNT_THRESHOLD` number
+            of times till we start using it. Otherwise the previous stable state is
+            used.
+            '''
 
-        # add routine to collect images for labelling
-        if self.collect:
-            #TODO we need a filename generator?
-            #cv2.imwrite(, msg)
-            pass
+            # add routine to collect images for labelling
+            if self.collect:
+                #TODO we need a filename generator?
+                #cv2.imwrite(, msg)
+                pass
 
-        if self.state != state:
-            self.state_count = 0
-            self.state = state
-        elif self.state_count >= STATE_COUNT_THRESHOLD:
-            self.last_state = self.state
-            light_wp = light_wp if state == TrafficLight.RED else -1
-            self.last_wp = light_wp
-            self.upcoming_red_light_pub.publish(Int32(light_wp))
+            if self.state != state:
+                self.state_count = 0
+                self.state = state
+            elif self.state_count >= STATE_COUNT_THRESHOLD:
+                self.last_state = self.state
+                light_wp = light_wp if state == TrafficLight.RED else -1
+                self.last_wp = light_wp
+                self.upcoming_red_light_pub.publish(Int32(light_wp))
+            else:
+                self.upcoming_red_light_pub.publish(Int32(self.last_wp))
+            self.state_count += 1
+
+        elif self.image_buffer <= IMAGE_BUFFER:
+            self.image_buffer += 1
         else:
-            self.upcoming_red_light_pub.publish(Int32(self.last_wp))
-        self.state_count += 1
-
-        #elif self.image_buffer <= IMAGE_BUFFER:
-        #    self.image_buffer += 1
-        #else:
-        #    self.image_buffer = 0
+            self.image_buffer = 0
 
 
     def get_closest_waypoint(self, x, y):
